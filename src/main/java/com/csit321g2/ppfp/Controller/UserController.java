e8d5cc2b0e6004e145df5a551b4787702e217a78
JohnRodolphBacalso <johnrodolph.bacalso@cit.edu>
2024-05-06 21:24:10 +0800

    Updated and Deleted things like imports and others

--- src/main/java/com/csit321g2/ppfp/Controller/UserController.java
+++ src/main/java/com/csit321g2/ppfp/Controller/UserController.java
@@ -4,8 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csit321g2.ppfp.Entity.UserEntity;
-import com.csit321g2.ppfp.Repository.UserRepository;
+
 import com.csit321g2.ppfp.Service.UserService;
 
 @RestController
